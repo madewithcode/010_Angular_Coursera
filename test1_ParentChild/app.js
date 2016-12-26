b07c04907e67a2234f4780129142f123c25f50fa
madewithcode <ahuja.software@gmail.com>
2016-12-25 22:19:43 -0500

    parent child scope

--- test1_ParentChild/app.js
+++ test1_ParentChild/app.js
@@ -6,15 +6,27 @@
   console.log('myApp module created');
 
   // controller
-  myApp.controller('myController', myController);
+  myApp.controller('myController1', myController1);
+  myApp.controller('myController2', myController2);
 
   // inject
-  myController.$inject = ['$scope'];
+  myController1.$inject = ['$scope'];
+  myController2.$inject = ['$scope'];
 
   // controller function
-  function myController($scope) {
-    console.log('myController()');
-    $scope.message = 'hello, world!!';
+  function myController1($scope) {
+    console.log('myController1()');
+    $scope.c = 'i am controller 1';
+    console.log($scope);
+    console.log('-------------------------');
   }
+
+  function myController2($scope) {
+    console.log('myController2()');
+    $scope.c = 'i am controller 2';
+    console.log($scope);
+    console.log('-------------------------');
+  }
+
 
 })();
